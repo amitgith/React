cb541c5003455c7d3a1ab56bb7755c086c627295
Amit Kumar Sharma <ak.sharma9340038@gmail.com>
2026-07-03 15:32:44 +0530

    "form handling - optimized"

--- day6-revision/src/components/About.jsx
+++ day6-revision/src/components/About.jsx
@@ -1,9 +1,42 @@
-import React from "react";
+import React, { useState } from "react";
 
 const About = () => {
-  return <div className="h-[100px] bg-red-500">
-    <h1>This is a About</h1>
-  </div>;
+  const [formdata, SetFormData] = useState({});
+  console.log(formdata);
+  let handleClick = (e) => {
+    let { name, value } = e.target;
+    SetFormData({ ...formdata, [name]: value });
+  };
+
+  return (
+    <div className="flex flex-col gap-5 w-60">
+      <input
+        name="name"
+        onChange={handleClick}
+        className="border-2"
+        type="text"
+        placeholder="Name"
+      />
+      <input
+        name="email"
+        onChange={handleClick}
+        className="border-2"
+        type="text"
+        placeholder="Email"
+      />
+      <input
+        name="password"
+        onChange={handleClick}
+        className="border-2"
+        type="text"
+        placeholder="Password"
+      />
+      <button className="border-2">Submit</button>
+      <h1>This is Name - {formdata.name}</h1>
+      <h1>This is Email - {formdata.email}</h1>
+      <h1>This is Password - {formdata.password}</h1>
+    </div>
+  );
 };
 
 export default About;
--- day6-revision/src/main.jsx
+++ day6-revision/src/main.jsx
@@ -2,7 +2,8 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 // import Web from "./components/Web";
 // import Cards from "./components/Cards";
-import Contacts from "./components/Contacts";
+// import Contacts from "./components/Contacts";
+import About from "./components/About";
 // import App from "./App.jsx";
 
-createRoot(document.getElementById("root")).render(<Contacts />);
+createRoot(document.getElementById("root")).render(<About />);
