9184c30ed99656de6524a028ef26fc6ccba1e5ae
Amit Kumar Sharma <ak.sharma9340038@gmail.com>
2026-07-03 15:09:48 +0530

    "form handling -Beter Approach "

--- day6-revision/src/components/Contacts.jsx
+++ day6-revision/src/components/Contacts.jsx
@@ -1,9 +1,39 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Contacts = () => {
+  const [formdata, SetFormData] = useState({});
+  console.log(formdata);
+
   return (
-    <div className="h-[100px] bg-blue-500">
-      <h1>This is a Contacts</h1>
+    <div className="flex flex-col gap-5 w-60">
+      <input
+        onChange={(e) => {
+          SetFormData({ ...formdata, name: e.target.value });
+        }}
+        className="border-2"
+        type="text"
+        placeholder="Name"
+      />
+      <input
+        onChange={(e) => {
+          SetFormData({ ...formdata, email: e.target.value });
+        }}
+        className="border-2"
+        type="text"
+        placeholder="Email"
+      />
+      <input
+        onChange={(e) => {
+          SetFormData({ ...formdata, password: e.target.value });
+        }}
+        className="border-2"
+        type="text"
+        placeholder="Password"
+      />
+      <button className="border-2">Submit</button>
+      <h1>This is Name - {formdata.name}</h1>
+      <h1>This is Email - {formdata.email}</h1>
+      <h1>This is Password - {formdata.password}</h1>
     </div>
   );
 };
--- day6-revision/src/main.jsx
+++ day6-revision/src/main.jsx
@@ -1,7 +1,8 @@
 import { createRoot } from "react-dom/client";
 import "./index.css";
 // import Web from "./components/Web";
-import Cards from "./components/Cards";
+// import Cards from "./components/Cards";
+import Contacts from "./components/Contacts";
 // import App from "./App.jsx";
 
-createRoot(document.getElementById("root")).render(<Cards />);
+createRoot(document.getElementById("root")).render(<Contacts />);
