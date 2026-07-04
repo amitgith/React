ed0a6aa0ef078081e79ee0ac31a2aa4dac34d41d
Amit Kumar Sharma <ak.sharma9340038@gmail.com>
2026-07-04 13:42:41 +0530

    "Basic login and Register Form"

---
+++ day7-revision/src/components/Login.jsx
@@ -0,0 +1,37 @@
+import React from "react";
+
+const Login = ({ setToggle }) => {
+  return (
+    <div className="bg-white w-90 p-6 rounded flex flex-col gap-4">
+      <h1 className="text-center text-xl">Login</h1>
+      <form action="" className="flex flex-col gap-4">
+        <input
+          className="p-2 border border-gray-400 rounded"
+          type="text"
+          placeholder="Email"
+        />
+        <input
+          className="p-2 border border-gray-400 rounded"
+          type="password"
+          placeholder="Password"
+        />
+        <button className="p-2 bg-blue-500 text-white rounded hover:bg-blue-600 cursor-pointer">
+          Login
+        </button>
+      </form>
+      <p>
+        Didn't have an Account?{" "}
+        <span
+          onClick={() => {
+            setToggle(false);
+          }}
+          className="text-blue-600 cursor-pointer hover:text-blue-700"
+        >
+          Register here
+        </span>
+      </p>
+    </div>
+  );
+};
+
+export default Login;
---
+++ day7-revision/src/components/Register.jsx
@@ -0,0 +1,42 @@
+import React from "react";
+
+const Register = ({ setToggle }) => {
+  return (
+    <div className="bg-white w-90 p-6 rounded flex flex-col gap-4">
+      <h1 className="text-center text-xl">Register</h1>
+      <form action="" className="flex flex-col gap-4">
+        <input
+          className="p-2 border border-gray-400 rounded"
+          type="text"
+          placeholder="Name"
+        />
+        <input
+          className="p-2 border border-gray-400 rounded"
+          type="text"
+          placeholder="Email"
+        />
+        <input
+          className="p-2 border border-gray-400 rounded"
+          type="password"
+          placeholder="Password"
+        />
+        <button className="p-2 bg-blue-500 text-white rounded hover:bg-blue-600 cursor-pointer">
+          Register
+        </button>
+      </form>
+      <p>
+        Already have an Account?
+        <span
+          onClick={() => {
+            setToggle(true);
+          }}
+          className="text-blue-600 cursor-pointer hover:text-blue-700"
+        >
+          Login here
+        </span>
+      </p>
+    </div>
+  );
+};
+
+export default Register;
