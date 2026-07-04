5841e142458c48f0a40dc29c3b13d7d483887bb6
Amit Kumar Sharma <ak.sharma9340038@gmail.com>
2026-07-04 14:11:55 +0530

    "input handling"

--- day7-revision/src/components/Register.jsx
+++ day7-revision/src/components/Register.jsx
@@ -1,21 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Register = ({ setToggle }) => {
+  const [formdata, setFormData] = useState({});
+  const handleClick = (e) => {
+    const { name, value } = e.target;
+    setFormData({ ...formdata, [name]: value });
+  };
   return (
     <div className="bg-white w-90 p-6 rounded flex flex-col gap-4">
       <h1 className="text-center text-xl">Register</h1>
       <form action="" className="flex flex-col gap-4">
         <input
+          onChange={handleClick}
           className="p-2 border border-gray-400 rounded"
           type="text"
           placeholder="Name"
         />
         <input
+          onChange={handleClick}
           className="p-2 border border-gray-400 rounded"
           type="text"
           placeholder="Email"
         />
         <input
+          onChange={handleClick}
           className="p-2 border border-gray-400 rounded"
           type="password"
           placeholder="Password"
