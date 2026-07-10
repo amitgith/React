b16a6b07c242991a09eaa7f93324c2fda89a563a
Amit Kumar Sharma <ak.sharma9340038@gmail.com>
2026-07-10 12:15:57 +0530

    useRef use

--- day8-useRef -reack-hook-state/src/components/Form.jsx
+++ day8-useRef -reack-hook-state/src/components/Form.jsx
@@ -1,34 +1,50 @@
-import React, { useState } from "react";
+import React, { useRef, useState } from "react";
 
 const Form = () => {
+  // useRef;
   console.log("app rendering");
 
-  const [formData, setFormData] = useState({});
-  console.log(formData);
+  // const [formData, setFormData] = useState({});
+  // // console.log(formData);
+  let inpRef = useRef();
+  console.log(inpRef);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(inpRef);
+  };
 
   return (
     <div className="w-80 bg-white">
-      <form className="flex flex-col gap-4 p-6">
+      <form className="flex flex-col gap-4 p-6" onSubmit={handleSubmit}>
         <input
-          onChange={(e) =>
-            setFormData({ ...formData, productName: e.target.value })
-          }
+          ref={inpRef}
+          // onChange={(e) =>
+          //   setFormData({ ...formData, productName: e.target.value })
+          // }
           className="p-2 border border-gray-400 rounded"
           type="text"
           placeholder="Product name"
         />
         <input
+          // onChange={(e) => setFormData({ ...formData, price: e.target.value })}
           className="p-2 border border-gray-400 rounded"
           type="number"
           placeholder="Price"
         />
         <span>Select category</span>
-        <select className="p-2 border border-gray-400 rounded">
+        <select
+          className="p-2 border border-gray-400 rounded"
+          // onChange={(e) =>
+          //   setFormData({ ...formData, category: e.target.value })
+          // }
+        >
           <option value="MENS">Mens</option>
           <option value="WOMEN">Women</option>
           <option value="KIDS">Kids</option>
         </select>
         <input
+          // onChange={(e) => setFormData({ ...formData, Image: e.target.value })}
           className="p-2 border border-gray-400 rounded"
           type="url"
           placeholder="Image URL"
