ffdff0606ca5e9da3fdab131fc8e2c70d73bc8fb
Amit Kumar Sharma <ak.sharma9340038@gmail.com>
2026-07-11 07:18:07 +0530

    creating folder day8

--- day8-useRef -reack-hook-state/src/components/Form.jsx
+++ day8-useRef -reack-hook-state/src/components/Form.jsx
@@ -1,50 +1,32 @@
 import React, { useRef, useState } from "react";
 
 const Form = () => {
-  // useRef;
   console.log("app rendering");
 
-  // const [formData, setFormData] = useState({});
-  // // console.log(formData);
-  let inpRef = useRef();
+  const inpRef = useRef();
   console.log(inpRef);
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    console.log(inpRef);
-  };
 
   return (
     <div className="w-80 bg-white">
-      <form className="flex flex-col gap-4 p-6" onSubmit={handleSubmit}>
+      <form className="flex flex-col gap-4 p-6">
         <input
-          ref={inpRef}
-          // onChange={(e) =>
-          //   setFormData({ ...formData, productName: e.target.value })
-          // }
+          ref={(e) => (inpRef.current.productName = e)}
           className="p-2 border border-gray-400 rounded"
           type="text"
           placeholder="Product name"
         />
         <input
-          // onChange={(e) => setFormData({ ...formData, price: e.target.value })}
           className="p-2 border border-gray-400 rounded"
           type="number"
           placeholder="Price"
         />
         <span>Select category</span>
-        <select
-          className="p-2 border border-gray-400 rounded"
-          // onChange={(e) =>
-          //   setFormData({ ...formData, category: e.target.value })
-          // }
-        >
+        <select className="p-2 border border-gray-400 rounded">
           <option value="MENS">Mens</option>
           <option value="WOMEN">Women</option>
           <option value="KIDS">Kids</option>
         </select>
         <input
-          // onChange={(e) => setFormData({ ...formData, Image: e.target.value })}
           className="p-2 border border-gray-400 rounded"
           type="url"
           placeholder="Image URL"
