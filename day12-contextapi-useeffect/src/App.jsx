827465870b86fd54debd3fcb20dcdaeb0863f323
Amit Kumar Sharma <ak.sharma9340038@gmail.com>
2026-07-14 13:24:55 +0530

    installed axios, fakestoreapi and use async , await

--- day12-contextapi-useeffect/src/App.jsx
+++ day12-contextapi-useeffect/src/App.jsx
@@ -3,10 +3,20 @@ import Home from "./components/Home";
 import About from "./components/About";
 import Contact from "./components/Contact";
 import { MyShop, MyShopProvider } from "./context/MyContext";
+import axios from "axios";
 
 const App = () => {
   const { count, setCount } = useContext(MyShop);
   const [toggle, setToggle] = useState(false);
+  const [apiData, setApiData] = useState(null);
+  let getData = async () => {
+    let res = await axios.get("https://fakestoreapi.com/products");
+    console.log(res.data);
+    setApiData(res.data);
+  };
+  useEffect(() => {
+    getData();
+  }, []);
   useEffect(() => {
     console.log("App rendering");
   }, [toggle]);
@@ -18,7 +28,7 @@ const App = () => {
         Change Toggle state
       </button>
       {/* <Home /> */}
-      {toggle ? <Contact /> : <About />}
+      {/* {toggle ? <Contact /> : <About />} */}
     </div>
   );
 };
