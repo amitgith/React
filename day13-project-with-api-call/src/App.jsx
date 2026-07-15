403a9d9e8eba16c97bccc824d6e10218b4e4510e
Amit Kumar Sharma <ak.sharma9340038@gmail.com>
2026-07-15 17:48:40 +0530

    installed axios use async,await,try and catch

--- day13-project-with-api-call/src/App.jsx
+++ day13-project-with-api-call/src/App.jsx
@@ -1,6 +1,15 @@
 import React from "react";
-
+import axios from "axios";
 const App = () => {
+  const getProductsData = async () => {
+    try {
+      let res = await axios.get("https://fakestoreapi.com/products");
+      console.log(res.data);
+    } catch (error) {
+      console.log("error in api", error);
+    }
+  };
+  getProductsData();
   return (
     <div>
       <h1>Hello</h1>
