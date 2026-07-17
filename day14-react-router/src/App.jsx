a6397bfd7aedd104a44b5eb40e5e97d5204c1b27
Amit Kumar Sharma <ak.sharma9340038@gmail.com>
2026-07-17 09:59:19 +0530

    another example of toggle , navbar switch

--- day14-react-router/src/App.jsx
+++ day14-react-router/src/App.jsx
@@ -4,22 +4,25 @@ import About from "./pages/About";
 import Contact from "./pages/Contact";
 
 const App = () => {
-  const [toggle, setToggle] = useState(false);
+  const [toggle, setToggle] = useState("Home");
   return (
     <div className="h-screen flex  flex-col bg-slate-400 text-3xl text-white">
       <nav className="flex justify-between items-center bg-black p-3 font-bold">
         LOGO
         <div className="flex gap-8 cursor-pointer">
-          <p onClick={() => setToggle(true)}>Home</p>
-          <p onClick={() => setToggle(false)}>About</p>
-          <p>Contact</p>
+          <p onClick={() => setToggle("home")}>Home</p>
+          <p onClick={() => setToggle("about")}>About</p>
+          <p onClick={() => setToggle("contact")}>Contact</p>
         </div>
         <button className="px-3 py-2 bg-amber-500 cursor-pointer rounded">
           Login
         </button>
       </nav>
-      {toggle ? <Home /> : <About />}
+      {/* {toggle ? <Home /> : <About />} */}
 
+      {toggle === "home" && <Home />}
+      {toggle === "about" && <About />}
+      {toggle === "contact" && <Contact />}
       {/* <Contact /> */}
     </div>
   );
