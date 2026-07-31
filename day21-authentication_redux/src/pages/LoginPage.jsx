a7d3e85ae8c9ae4013cc765f01533e2d6de8a999
Amit Kumar Sharma <ak.sharma9340038@gmail.com>
2026-07-31 20:35:41 +0530

    error solving

--- day21-authentication_redux/src/pages/LoginPage.jsx
+++ day21-authentication_redux/src/pages/LoginPage.jsx
@@ -1,11 +1,25 @@
 import React from "react";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
+import {
+  decrement,
+  increment,
+  incrementbyValue,
+} from "../features/counterSlice";
 
 const LoginPage = () => {
-    let {count} = useSelector9()
-  return <div>
-    <h1>Count is {count}</h1>
-  </div>;
+  let dispatch = useDispatch();
+  let { count } = useSelector((store) => store.counter);
+  return (
+    <div>
+      <h1>Count is {count}</h1>
+      <button onClick={() => dispatch(decrement())}>Decrement</button>
+      <button onClick={() => dispatch(increment())}>Increment</button>
+      <input type="text" placeholder="Enter count " />
+      <button onClick={() => dispatch(incrementbyValue(456))}>
+        Add to Count
+      </button>
+    </div>
+  );
 };
 
 export default LoginPage;
