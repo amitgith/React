e5bf82623ca69174f814ab425f7edc36bf76ac2f
Amit Kumar Sharma <ak.sharma9340038@gmail.com>
2026-08-04 16:35:18 +0530

    Setup Completed redux

---
+++ day23-ecomm-layered-architecture/src/features/auth/state/authSlice.jsx
@@ -0,0 +1,25 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+export const authSlice = createSlice({
+  name: "auth",
+  initialState: {
+    user: null,
+    isAuthenticated: false,
+    isLoading: true,
+  },
+  reducers: {
+    addUser: (state, action) => {
+      state.user = action.payload;
+      state.isAuthenticated = true;
+      //   state.isLoading = true;
+    },
+    removeUser: (state) => {
+      state.user = null;
+      state.isAuthenticated = false;
+      //   state.isLoading = false;
+    },
+  },
+});
+
+export const { addUser, removeUser } = authSlice.actions;
+export default authSlice.reducer;
--- day23-ecomm-layered-architecture/src/main.jsx
+++ day23-ecomm-layered-architecture/src/main.jsx
@@ -1,7 +1,13 @@
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+import { store } from "./app/store.jsx";
+import { Provider } from "react-redux";
+import toast, { Toaster } from "react-hot-toast";
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
+  <Provider store={store}>
     <App />
-)
+    <Toaster />
+  </Provider>,
+);
