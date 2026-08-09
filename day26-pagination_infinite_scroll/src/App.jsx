c35df52ad58e137ca17605fcb2a755abbc5b40ab
Amit Kumar Sharma <ak.sharma9340038@gmail.com>
2026-08-09 10:11:44 +0530

    pagination normal approach

--- day26-pagination_infinite_scroll/src/App.jsx
+++ day26-pagination_infinite_scroll/src/App.jsx
@@ -3,25 +3,57 @@ import React, { useEffect, useState } from "react";
 import RecipeCard from "./components/ReceipeCard";
 
 const App = () => {
-  const [receipes, setReceipes] = useState(null);
-  console.log(receipes);
+  const [recipes, setRecipes] = useState(null);
+  console.log(recipes);
+  const [page, setPage] = useState(0);
+  console.log(page);
 
-  const getAllReceipes = async () => {
+  let limit = 10;
+
+  const getAllRecipes = async () => {
     try {
-      let res = await axios.get("https://dummyjson.com/recipes");
-      setReceipes(res.data);
+      console.log("Api calling....");
+
+      let res = await axios.get(
+        `https://dummyjson.com/recipes?limit=${limit}&skip=${page * limit}`,
+      );
+      setRecipes(res.data);
     } catch (error) {
       console.log("error in api", error);
     }
   };
   useEffect(() => {
-    getAllReceipes();
-  }, []);
+    getAllRecipes();
+  }, [page]);
+
+  let totalPages = recipes?.total / limit;
+
   return (
-    <div className="grid grid-cols-4 gap-3 mt-2">
-      {receipes?.recipes.map((val) => (
-        <RecipeCard key={val.id} recipe={val} />
-      ))}
+    <div className="flex flex-col gap-5 items-center">
+      <div className="grid w-full grid-cols-4 gap-3 mt-2">
+        {recipes?.recipes.map((val) => (
+          <RecipeCard key={val.id} recipe={val} />
+        ))}
+      </div>
+      <div className="flex gap-5 mb-5 items-center">
+        <button
+          disabled={page === 0}
+          onClick={() => setPage(page - 1)}
+          className="bg-red-500 text-white px-3 py-2 text-xl rounded cursor-pointer"
+        >
+          Prev
+        </button>
+        <p>
+          Page {page + 1} of {totalPages}
+        </p>
+        <button
+          disabled={page >= totalPages - 1}
+          onClick={() => setPage(page + 1)}
+          className="bg-red-500 text-white px-3 py-2 text-xl rounded cursor-pointer"
+        >
+          Next
+        </button>
+      </div>
     </div>
   );
 };
